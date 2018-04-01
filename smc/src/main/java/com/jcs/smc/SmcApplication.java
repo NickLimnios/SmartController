874eabd92330bc660e43c26716d8cd6611c8cf7c
Nick Limnios <nicklimnios@gmail.com>
2018-04-01 12:31:22 +0300

    Initial Project Commit

---
+++ smc/src/main/java/com/jcs/smc/SmcApplication.java
@@ -0,0 +1,12 @@
+package com.jcs.smc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmcApplication.class, args);
+	}
+}
